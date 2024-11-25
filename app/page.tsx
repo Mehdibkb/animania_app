6532e7cfe0d731e57530819c74434ecb0f5b0e07
Mehdibkb <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-11-25 10:48:06 +0100

    nouveaux fichiers et dossiers

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import { prisma } from "app/db/prisma";
+
+export default async function Home() {
+ await prisma.user.create({
+   data: {
+     name: "m",
+     email: "mlc@thtfhfthruc.com",
+     posts: {
+       create: {
+         title: "My First Post",
+       },
+     },
+   },
+ });
+ const allUsers = await prisma.user.findMany({
+   select: { name: true },
+ });
+ return (
+<div>
+<ul>
+       {allUsers.map((user) => (
+<li>{user.name}</li>
+       ))}
+</ul>
+</div>
+ );
+}
