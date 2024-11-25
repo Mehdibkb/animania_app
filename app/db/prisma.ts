659ad694bff16665b10f7ffd132e484252065f71
Mehdibkb <mehdi-yacine.boukabou@supdevinci-edu.fr>
2024-11-25 11:53:42 +0100

    prisma.ts

--- app/db/prisma.ts
+++ app/db/prisma.ts
@@ -1,18 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-  // eslint-disable-next-line no-var
+  // Evite les doublons lors de l'utilisation en mode développement
   var prisma: PrismaClient | undefined;
 }
 
+// Initialisation unique de Prisma Client
 export const prisma =
-  global.prisma ??
+  global.prisma ||
   new PrismaClient({
     log:
       process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn'] : ['error'],
+        ? ['query', 'info', 'warn', 'error'] // Log supplémentaire pour le développement
+        : ['error'], // Moins de logs en production
   });
 
+// Assurez-vous que Prisma n'est pas réinitialisé à chaque rechargement en développement
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
 }
